62357cd748b201d4898ab941da5670b8cd33dca0
romanandani266 <romanandani266@gmail.com>
2025-02-06 19:00:33 +0530

    Add app/main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-print('Hello from main.py')
+from fastapi import FastAPI
+from .database import engine, Base
+from .routers import auth, products, inventory, sales
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(auth.router)
+app.include_router(products.router)
+app.include_router(inventory.router)
+app.include_router(sales.router)
