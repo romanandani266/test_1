2fc175f831f8ba3288381fa6dd67521f682a3154
romanandani266 <romanandani266@gmail.com>
2025-02-06 18:53:09 +0530

    Add retail_inventory_management/app/crud.py

---
+++ retail_inventory_management/app/crud.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+from app import models, schemas
+from app.utils.security import get_password_hash
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(email=user.email, password_hash=hashed_password, role=user.role)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_product(db: Session, product_id: UUID):
+    return db.query(models.Product).filter(models.Product.id == product_id).first()
+
+def create_product(db: Session, product: schemas.ProductCreate):
+    db_product = models.Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
