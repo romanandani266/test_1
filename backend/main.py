dc3fc2cb329ea6e0c5ca09d620a78b37204a7220
romanandani266 <romanandani266@gmail.com>
2025-03-03 14:14:48 +0530

    Content edited for "backend/main.py"

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-from fastapi import FastAPI, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from typing import List, Optional
