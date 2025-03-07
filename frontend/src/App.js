85c58d3bf8d36c5d70830e48efd7022e09992957
romanandani266 <romanandani266@gmail.com>
2025-03-07 15:52:41 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,35 @@
-"// This is the main App component\n// This file defines the structure of the application\n// The App component serves as the root of the application\nimport Login from \"./components/Login\";\nimport Inventory from \"./components/Inventory\";\nimport UserRights from \"./components/UserRights\";\n\nfunction App() {\n  return (\n    <div>\n      <Navbar />\n      <Routes>\n        <Route path=\"/\" element={<Login />} />\n        <Route path=\"/inventory\" element={<Inventory />} />\n        <Route path=\"/user-rights\" element={<UserRights />} />\n      </Routes>\n    </div>\n  );\n}\n\nexport default App;"
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import HomePage from "./pages/HomePage";
+import CreateEditPage from "./pages/CreateEditPage";
+import BlogDetailPage from "./pages/BlogDetailPage";
+
+const App = () => {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            My Blog Platform
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/create">
+            Create Blog
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreateEditPage />} />
+        <Route path="/edit/:id" element={<CreateEditPage />} />
+        <Route path="/blogs/:id" element={<BlogDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
