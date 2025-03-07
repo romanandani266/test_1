98e353eba51f2b7da0865ccc7edd7a73c0df6de6
romanandani266 <romanandani266@gmail.com>
2025-03-07 11:12:01 +0530

    Add frontend/src/api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,52 +1,20 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "http://localhost:8080/api";
 
-export const getAllBlogs = async () => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/blogs`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching blogs:", error);
-    throw error;
-  }
-};
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export const getBlogById = async (id) => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/blogs/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching blog:", error);
-    throw error;
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-};
+  return config;
+});
 
-export const createBlog = async (blog) => {
-  try {
-    const response = await axios.post(`${API_BASE_URL}/blogs`, blog);
-    return response.data;
-  } catch (error) {
-    console.error("Error creating blog:", error);
-    throw error;
-  }
-};
-
-export const updateBlog = async (id, blog) => {
-  try {
-    const response = await axios.put(`${API_BASE_URL}/blogs/${id}`, blog);
-    return response.data;
-  } catch (error) {
-    console.error("Error updating blog:", error);
-    throw error;
-  }
-};
-
-export const deleteBlog = async (id) => {
-  try {
-    await axios.delete(`${API_BASE_URL}/blogs/${id}`);
-  } catch (error) {
-    console.error("Error deleting blog:", error);
-    throw error;
-  }
-};
+export default api;
