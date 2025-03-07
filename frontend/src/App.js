f1cfab398f85faf0f111f8bf0cc92c4b00c0154e
romanandani266 <romanandani266@gmail.com>
2025-03-07 12:54:53 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./theme";
 import Navbar from "./components/Navbar";
-import BlogList from "./components/BlogList";
-import BlogDetail from "./components/BlogDetail";
-import BlogForm from "./components/BlogForm";
+import Login from "./components/Login";
+import Inventory from "./components/Inventory";
+import UserRights from "./components/UserRights";
 
-const App = () => {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<BlogList />} />
-        <Route path="/blogs/:id" element={<BlogDetail />} />
-        <Route path="/create" element={<BlogForm />} />
-        <Route path="/edit/:id" element={<BlogForm />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/user-rights" element={<UserRights />} />
       </Routes>
-    </ThemeProvider>
+    </div>
   );
-};
+}
 
 export default App;
