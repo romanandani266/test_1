595308b686c9399ea45d8ec8435e1c35837ea1c9
romanandani266 <romanandani266@gmail.com>
2025-03-07 12:56:27 +0530

    Add frontend/src/components/UserRights.js

---
+++ frontend/src/components/UserRights.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+function UserRights() {
+  return (
+    <Container>
+      <Typography variant="h4">User Rights</Typography>
+      <Typography>
+        Implement user rights functionality here (e.g., Right to Know, Right to Delete, etc.).
+      </Typography>
+    </Container>
+  );
+}
+
+export default UserRights;
