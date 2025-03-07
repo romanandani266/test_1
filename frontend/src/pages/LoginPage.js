628f2a0e69dcc9d8fd73a0d2579b182d7dc84061
romanandani266 <romanandani266@gmail.com>
2025-03-07 15:24:35 +0530

    Content edited for "frontend/src/pages/LoginPage.js"

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,16 +1,1 @@
-import React, { createContext, useState } from 'react';
-
-export const AuthContext = createContext();
-
-export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState(null);
-
-  const login = (data) => setAuth(data);
-  const logout = () => setAuth(null);
-
-  return (
-    <AuthContext.Provider value={{ auth, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+"// This is the main App component\n// This file defines the structure of the application\nimport Login from \"./components/Login\";\nimport Inventory from \"./components/Inventory\";\nimport UserRights from \"./components/UserRights\";\n\nfunction App() {\n  return (\n    <div>\n      <Navbar />\n      <Routes>\n        <Route path=\"/\" element={<Login />} />\n        <Route path=\"/inventory\" element={<Inventory />} />\n        <Route path=\"/user-rights\" element={<UserRights />} />\n      </Routes>\n    </div>\n  );\n}\n\nexport default App;"
