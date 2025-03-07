90fdf7cda1565124045b6c678068bba23ff24641
romanandani266 <romanandani266@gmail.com>
2025-03-07 12:09:04 +0530

    Add frontend/src/pages/HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { getAllBlogs } from "../api";
+import { Grid } from "@mui/material";
 import BlogCard from "../components/BlogCard";
-import { Grid, Container } from "@mui/material";
+import { getAllBlogs } from "../api";
 
 const HomePage = () => {
   const [blogs, setBlogs] = useState([]);
@@ -19,15 +19,13 @@ const HomePage = () => {
   }, []);
 
   return (
-    <Container>
-      <Grid container spacing={3}>
-        {blogs.map((blog) => (
-          <Grid item xs={12} sm={6} md={4} key={blog.id}>
-            <BlogCard blog={blog} />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <Grid container spacing={2} sx={{ padding: 2 }}>
+      {blogs.map((blog) => (
+        <Grid item xs={12} sm={6} md={4} key={blog.id}>
+          <BlogCard blog={blog} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
