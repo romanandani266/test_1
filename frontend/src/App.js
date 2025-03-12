062763bbd758bac383f96b4d5a3ca2f19152c9a4
romanandani266 <romanandani266@gmail.com>
2025-03-12 10:47:03 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import theme from './theme';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import CreateEditBlog from './pages/CreateEditBlog';
-import BlogDetailPage from './pages/BlogDetailPage';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { CssBaseline, AppBar, Toolbar, Typography, Button } from "@mui/material";
+import HomePage from "./pages/HomePage";
+import CreateEditPage from "./pages/CreateEditPage";
+import BlogDetailPage from "./pages/BlogDetailPage";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <CssBaseline />
-      <Navbar />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            My Blog App
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/create">
+            Create Blog
+          </Button>
+        </Toolbar>
+      </AppBar>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateEditBlog />} />
-        <Route path="/edit/:id" element={<CreateEditBlog />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreateEditPage />} />
+        <Route path="/edit/:id" element={<CreateEditPage />} />
         <Route path="/blogs/:id" element={<BlogDetailPage />} />
       </Routes>
-    </ThemeProvider>
+    </>
   );
 }
 
