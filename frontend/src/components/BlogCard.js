cffb71f754431be62a917785ce045498f46b6330
romanandani266 <romanandani266@gmail.com>
2025-03-13 18:03:44 +0530

    Add frontend/src/components/BlogCard.js

--- frontend/src/components/BlogCard.js
+++ frontend/src/components/BlogCard.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 function BlogCard({ blog }) {
   return (
-    <Card>
+    <Card style={{ marginBottom: "20px" }}>
       <CardMedia component="img" height="140" image={blog.image_url} alt={blog.title} />
       <CardContent>
-        <Typography variant="h6">{blog.title}</Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="h5">{blog.title}</Typography>
+        <Typography variant="body2" color="textSecondary">
           {blog.content.substring(0, 100)}...
         </Typography>
-        <Button component={Link} to={`/blogs/${blog.id}`}>
+        <Button component={Link} to={`/blogs/${blog.id}`} size="small">
           Read More
         </Button>
       </CardContent>
