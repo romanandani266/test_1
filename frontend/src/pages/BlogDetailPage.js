d1730da73585751823d0ef2acdf96893e3c19d32
romanandani266 <romanandani266@gmail.com>
2025-03-13 18:03:51 +0530

    Add frontend/src/pages/BlogDetailPage.js

--- frontend/src/pages/BlogDetailPage.js
+++ frontend/src/pages/BlogDetailPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getBlogById } from "../api";
-import { Container, Typography, Box } from "@mui/material";
+import { Typography, Container } from "@mui/material";
 
 function BlogDetailPage() {
   const { id } = useParams();
@@ -19,25 +19,17 @@ function BlogDetailPage() {
     fetchBlog();
   }, [id]);
 
-  if (!blog) return <Typography>Loading...</Typography>;
+  if (!blog) return <div>Loading...</div>;
 
   return (
-    <Container>
-      <Box
-        sx={{
-          backgroundImage: `url(https://plus.unsplash.com/premium_photo-1684581214880-2043e5bc8b8b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          padding: "50px",
-          color: "white",
-        }}
-      >
-        <Typography variant="h3">{blog.title}</Typography>
-        <Typography variant="subtitle1">{new Date(blog.created_at).toLocaleDateString()}</Typography>
-      </Box>
-      <img src={blog.image_url} alt={blog.title} style={{ width: "100%", marginTop: "20px" }} />
-      <Typography variant="body1" sx={{ mt: 3 }}>
-        {blog.content}
+    <Container style={{ padding: "20px", backgroundImage: `url(https://plus.unsplash.com/premium_photo-1684581214880-2043e5bc8b8b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)` }}>
+      <Typography variant="h3" style={{ marginBottom: "20px" }}>
+        {blog.title}
+      </Typography>
+      <img src={blog.image_url} alt={blog.title} style={{ width: "100%", marginBottom: "20px" }} />
+      <Typography variant="body1">{blog.content}</Typography>
+      <Typography variant="caption" display="block" style={{ marginTop: "20px" }}>
+        Created At: {new Date(blog.created_at).toLocaleString()}
       </Typography>
     </Container>
   );
