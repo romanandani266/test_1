c7c788ea55b14f8460697733048f25e76f5529d8
romanandani266 <romanandani266@gmail.com>
2025-03-19 20:53:32 +0530

    Add frontend/src/api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,53 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080";
+const API_BASE_URL = "http://localhost:8080/api";
 
-export const fetchBlogs = async () => {
-  try {
-    const response = await axios.get(`${API_URL}/blogs`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching blogs:", error);
-    throw error;
-  }
-};
-
-export const fetchBlogById = async (id) => {
-  try {
-    const response = await axios.get(`${API_URL}/blogs/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching blog:", error);
-    throw error;
-  }
-};
-
-export const createBlog = async (blog) => {
-  try {
-    const response = await axios.post(`${API_URL}/blogs`, blog);
-    return response.data;
-  } catch (error) {
-    console.error("Error creating blog:", error);
-    throw error;
-  }
-};
-
-export const updateBlog = async (id, blog) => {
-  try {
-    const response = await axios.put(`${API_URL}/blogs/${id}`, blog);
-    return response.data;
-  } catch (error) {
-    console.error("Error updating blog:", error);
-    throw error;
-  }
-};
-
-export const deleteBlog = async (id) => {
-  try {
-    const response = await axios.delete(`${API_URL}/blogs/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error("Error deleting blog:", error);
-    throw error;
-  }
+export const api = {
+  login: (data) => axios.post(`${API_BASE_URL}/login`, data),
+  getInventory: () => axios.get(`${API_BASE_URL}/inventory`),
+  updateInventory: (data) => axios.put(`${API_BASE_URL}/inventory`, data),
+  addProduct: (data) => axios.post(`${API_BASE_URL}/inventory`, data),
+  deleteProduct: (productId) => axios.delete(`${API_BASE_URL}/inventory/${productId}`),
+  getSalesTrends: () => axios.get(`${API_BASE_URL}/sales-trends`),
+  getNotifications: () => axios.get(`${API_BASE_URL}/notifications`),
 };
