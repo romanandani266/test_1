269b8982ecdd411a3a39fbe4bec4b09452189008
romanandani266 <romanandani266@gmail.com>
2025-03-19 19:40:00 +0530

    Add frontend/src/components/Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          My Blog App
+        </Typography>
+        <Button color="inherit" component={Link} to="/">
+          Home
+        </Button>
+        <Button color="inherit" component={Link} to="/create">
+          Create Blog
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
