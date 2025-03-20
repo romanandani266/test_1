4b2f3abe845aad1f0bffe120e7690147b86511c6
romanandani266 <romanandani266@gmail.com>
2025-03-20 16:25:36 +0530

    Add frontend/src/components/BlogCard.js

--- frontend/src/components/BlogCard.js
+++ frontend/src/components/BlogCard.js
@@ -1,19 +1,15 @@
-import React from "react";
-import { Card, CardMedia, CardContent, Typography, Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const BlogCard = ({ blog }) => {
   return (
     <Card>
       <CardMedia component="img" height="140" image={blog.image_url} alt={blog.title} />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {blog.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {blog.content.substring(0, 100)}...
-        </Typography>
-        <Button component={Link} to={`/blogs/${blog.id}`}>
+        <Typography variant="h5">{blog.title}</Typography>
+        <Typography variant="body2">{blog.content.substring(0, 100)}...</Typography>
+        <Button component={Link} to={`/blog/${blog.id}`}>
           Read More
         </Button>
       </CardContent>
