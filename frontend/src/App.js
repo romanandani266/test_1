ebc105a3f99f19d7d983900c6a94885cfef4aeaf
romanandani266 <romanandani266@gmail.com>
2025-03-21 12:07:16 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,28 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import { AppBar, Toolbar, Button, Container } from "@mui/material";
-import Inventory from "./components/Inventory";
-import Login from "./components/Login";
-import RestockingAlerts from "./components/RestockingAlerts";
-import SalesTrends from "./components/SalesTrends";
-import Notifications from "./components/Notifications";
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Button, Container } from '@mui/material';
+import Login from './components/Login';
+import Inventory from './components/Inventory';
+import Notifications from './components/Notifications';
+import Sales from './components/Sales';
 
 const App = () => {
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/inventory">
-            Inventory
-          </Button>
-          <Button color="inherit" component={Link} to="/alerts">
-            Restocking Alerts
-          </Button>
-          <Button color="inherit" component={Link} to="/sales-trends">
-            Sales Trends
-          </Button>
-          <Button color="inherit" component={Link} to="/notifications">
-            Notifications
-          </Button>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/inventory">Inventory</Button>
+          <Button color="inherit" component={Link} to="/notifications">Notifications</Button>
+          <Button color="inherit" component={Link} to="/sales">Sales</Button>
         </Toolbar>
       </AppBar>
-
       <Container>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/inventory" element={<Inventory />} />
-          <Route path="/alerts" element={<RestockingAlerts />} />
-          <Route path="/sales-trends" element={<SalesTrends />} />
           <Route path="/notifications" element={<Notifications />} />
+          <Route path="/sales" element={<Sales />} />
         </Routes>
       </Container>
     </div>
