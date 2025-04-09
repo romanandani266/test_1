4b8e74b48aee83d441faaaadfe3e14e940cd7b18
romanandani266 <romanandani266@gmail.com>
2025-04-09 18:41:44 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,46 @@
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Button, Container } from '@mui/material';
-import Login from './components/Login';
-import Inventory from './components/Inventory';
-import Notifications from './components/Notifications';
-import Sales from './components/Sales';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Container, AppBar, Toolbar, Button } from "@mui/material";
+import Inventory from "./components/Inventory";
+import Alerts from "./components/Alerts";
+import SalesTrends from "./components/SalesTrends";
+import Login from "./components/Login";
+import Integration from "./components/Integration";
 
 const App = () => {
   return (
-    <div>
+    <Container>
       <AppBar position="static">
         <Toolbar>
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/inventory">Inventory</Button>
-          <Button color="inherit" component={Link} to="/notifications">Notifications</Button>
-          <Button color="inherit" component={Link} to="/sales">Sales</Button>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/inventory">
+            Inventory
+          </Button>
+          <Button color="inherit" component={Link} to="/alerts">
+            Alerts
+          </Button>
+          <Button color="inherit" component={Link} to="/sales-trends">
+            Sales Trends
+          </Button>
+          <Button color="inherit" component={Link} to="/integration">
+            Integration
+          </Button>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
-      <Container>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/inventory" element={<Inventory />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/sales" element={<Sales />} />
-        </Routes>
-      </Container>
-    </div>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to Retail Inventory Management</h1>} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/alerts" element={<Alerts />} />
+        <Route path="/sales-trends" element={<SalesTrends />} />
+        <Route path="/integration" element={<Integration />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Container>
   );
 };
 
