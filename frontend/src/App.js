549269f1d1118cd9a8b772fc72df9697e730486f
romanandani266 <romanandani266@gmail.com>
2025-04-10 15:18:41 +0530

    Add frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import { Container, AppBar, Toolbar, Button } from "@mui/material";
+import { AppBar, Toolbar, Button, Container } from "@mui/material";
 import Inventory from "./components/Inventory";
 import Alerts from "./components/Alerts";
 import SalesTrends from "./components/SalesTrends";
 import Login from "./components/Login";
-import Integration from "./components/Integration";
 
 const App = () => {
   return (
-    <Container>
+    <div>
       <AppBar position="static">
         <Toolbar>
           <Button color="inherit" component={Link} to="/">
@@ -24,23 +23,22 @@ const App = () => {
           <Button color="inherit" component={Link} to="/sales-trends">
             Sales Trends
           </Button>
-          <Button color="inherit" component={Link} to="/integration">
-            Integration
-          </Button>
           <Button color="inherit" component={Link} to="/login">
             Login
           </Button>
         </Toolbar>
       </AppBar>
-      <Routes>
-        <Route path="/" element={<h1>Welcome to Retail Inventory Management</h1>} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/alerts" element={<Alerts />} />
-        <Route path="/sales-trends" element={<SalesTrends />} />
-        <Route path="/integration" element={<Integration />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Container>
+
+      <Container>
+        <Routes>
+          <Route path="/" element={<h1>Welcome to Retail Inventory Management</h1>} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/alerts" element={<Alerts />} />
+          <Route path="/sales-trends" element={<SalesTrends />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Container>
+    </div>
   );
 };
 
